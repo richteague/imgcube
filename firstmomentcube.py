5bd579ef735a28d117e7dc13ee4eefc2d700cbd7
richteague <richarddteague@gmail.com>
2018-10-05 10:53:25 -0400

    better minimization

--- firstmomentcube.py
+++ firstmomentcube.py
@@ -11,7 +11,7 @@ class firstmomentcube(imagecube):
 
     def __init__(self, path, mstar=None, inc=None, dist=None, vlsr=None,
                  resample=0, clip=None, collapse='quadratic', excludepix=None,
-                 verbose=True, suppress_warnings=True, error=None):
+                 verbose=True, suppress_warnings=True, error=None, **kwargs):
         """Read in the first moment map."""
 
         # Base class.
@@ -30,12 +30,12 @@ class firstmomentcube(imagecube):
                     mask = np.logical_and(self.data <= pix[0],
                                           self.data >= pix[1])
                 self.data = np.where(mask, self.data, 0.0)
-            self.data, self.error = self._collapse_cube(method=collapse)
+            self.data, self.error = self._collapse_cube(method=collapse,
+                                                        **kwargs)
         else:
             if error is None and self.verbose:
                 print("WARNING: No error specified. Assuming 0.1 km/s.")
             self.error = error if error is not None else 0.1
-        self.error = np.where(np.isfinite(self.error), self.error, 1e10)
 
         # Populate the parameters.
         if mstar is None:
@@ -55,88 +55,12 @@ class firstmomentcube(imagecube):
         else:
             self.vlsr = vlsr
             if self.vlsr > 1e3:
-                ("WARNING: systemic velocity in [m/s], not [km/s].")
+                print("WARNING: Converting VLSR in [m/s] to [km/s].")
+                self.vlsr *= 1e-3
 
-    def optimized_keplerian(self, p0=None, fit_mstar=True, beam=True,
-                            r_min=None, r_max=None, z_type='thin',
-                            nearest=None, optimize=True,
-                            Ninner=10, Nouter=3, step=1e-5, **kwargs):
-        """
-        Return [p0, theta_fixed, error] which maximize likelihood. This is done
-        through several iterations (specified by Ninner and Npouter) of
-        scipy.minimize where the parameters which are minimized are alternated.
-        This should provide a quicker (and better) estimate for the parameters.
-        """
-
-        # Check the input parameters.
-        z_type = z_type.lower()
-        if z_type not in ['thin', 'conical', 'flared']:
-            raise ValueError("Can only fit 'thin', 'conical' or 'flared'.")
-        theta_fixed = self.inc if fit_mstar else self.mstar
-
-        # Warning about the bounds.
-        r_min = 0.0 if r_min is None else r_min
-        if r_max is None and self.verbose:
-            print("WARNING: No r_max specified which may cause trouble.")
-        r_max = np.inf if r_max is None else r_max
-
-        # Make sure there are errors
-        if self.verbose and self.error is None:
-            print("WARNING: No error specified. Assuming 0.1 km/s.")
-        error = 0.1 if self.error is None else self.error
-        ivar = np.ones(self.data.shape) / error**2
-        if ivar.shape != self.data.shape:
-            raise ValueError("Inverse variance doesn't match data.shape.")
-
-        # Find the starting positions.
-        if p0 is None:
-            if self.verbose:
-                print("WARNING: Estimating starting values. May not work.")
-            free_theta = self.mstar if fit_mstar else self.inc
-            p0 = [0.0, 0.0, free_theta, self._estimate_PA(),
-                  np.nanmedian(self.data) * 1e3]
-            if z_type == 'conical':
-                p0 += [8.0]
-                if nearest is None:
-                    p0 += [0.0]
-            elif z_type == 'flared':
-                p0 += [0.3, 1.25]
-                if nearest is None:
-                    p0 += [0.0]
-            if self.verbose:
-                print("Have chosen:", p0)
-
-        # Calculate the inverse variance.
-        self.ivar = np.power(self.error, -2.0)
-        rvals = self.disk_coords(p0[0], p0[1], self.inc, p0[3])[0]
-        mask = np.logical_and(rvals >= r_min, rvals <= r_max)
-        self.ivar[~mask] = 0.0
-
-        # Find optimal starting positions.
-        if optimize:
-            from scipy.optimize import minimize
-
-            def nlnL(theta):
-                """Negative loglikelihood."""
-                return -self._lnprobability(theta, theta_fixed, fit_mstar,
-                                            beam, r_min, r_max, z_type,
-                                            nearest)
-            res = minimize(nlnL, x0=p0, method="Nelder-Mead",
-                           options={'maxiter': 100000})
-            if res.success:
-                p0 = res.x
-            elif self.verbose:
-                print("WARNING: scipy.optimize did not converge.")
-        elif self.verbose:
-            print("No optimization calls requested.")
-
-        # Update the mask.
-        self.ivar = np.power(self.error, -2.0)
-        rvals = self.disk_coords(p0[0], p0[1], self.inc, p0[3])[0]
-        mask = np.logical_and(rvals >= r_min, rvals <= r_max)
-        self.ivar[~mask] = 0.0
-
-        return p0, theta_fixed
+        # Calculate the uncertainties.
+        self.error = np.where(np.isfinite(self.error), self.error, 1e10)
+        self.ivar = self._calculate_ivar([0.0, 0.0, 0.0, 0.0])
 
     def fit_keplerian(self, p0=None, fit_mstar=True, beam=True, r_min=None,
                       r_max=None, z_type='thin', nearest=None, nwalkers=None,
@@ -207,21 +131,26 @@ class firstmomentcube(imagecube):
         except:
             raise ValueError("Cannot find emcee.")
 
-        # Find the parameters which maximize the likelihood.
-        p0, theta_fixed = self.optimized_keplerian(
-            p0=p0, fit_mstar=fit_mstar, beam=beam, r_min=r_min, r_max=r_max,
-            z_type=z_type, nearest=nearest, optimize=optimize
-            )
-        if self.verbose:
-            print("Starting positions:")
-            print(p0)
+        # Initial starting positions.
+        p0, theta_fixed = self._guess_p0(p0=p0, fit_mstar=fit_mstar,
+                                         z_type=z_type, nearest=nearest)
+        self.ivar = self._calculate_ivar(p0=p0, r_min=r_min, r_max=r_max)
+
+        # Optimize positions if requested.
+        args = (theta_fixed, fit_mstar, beam, z_type, nearest)
+        if optimize:
+            p0 = self._optimize_p0(p0, *args)
+            self.ivar = self._calculate_ivar(p0=p0, r_min=r_min, r_max=r_max)
+        elif self.verbose:
+            print("No optimization calls requested.")
+
+        # Set up the MCMC parameters.
         ndim = len(p0)
         nwalkers = 2 * ndim if nwalkers is None else nwalkers
         init = np.squeeze(p0)
         p0 = self._random_p0(init, scatter, nwalkers)
 
         # Make sure all starting positions are valid.
-        args = (theta_fixed, fit_mstar, beam, r_min, r_max, z_type, nearest)
         mask = np.ones(len(p0), dtype=bool)
         lp = np.empty(len(p0))
         while np.any(mask):
@@ -229,7 +158,7 @@ class firstmomentcube(imagecube):
             lp[mask] = [self._lnprobability(p00, *args) for p00 in p0[mask]]
             mask = ~np.isfinite(lp)
 
-        # Define the labels.
+        # Define the labels for plotting.
         labels = [r'$x_0$', r'$y_0$', r'$M_{\star}$' if fit_mstar else r'$i$',
                   r'${\rm PA}$', r'$v_{\rm LSR}$']
         if z_type == 'conical':
@@ -238,7 +167,8 @@ class firstmomentcube(imagecube):
             labels += [r'$z\,/\,r$', r'$\phi$']
         if z_type != 'thin' and nearest is None:
             labels += [r'${\rm tilt}$']
-        assert len(labels) == ndim, "Mismatch in p0 and labels."
+        if len(labels) != ndim:
+            raise ValueError("Mismatch in p0 and labels.")
 
         # Run the MCMC.
         sampler = emcee.EnsembleSampler(nwalkers, ndim, self._lnprobability,
@@ -248,17 +178,17 @@ class firstmomentcube(imagecube):
         samples = samples.reshape(-1, samples.shape[-1])
         samples[:, 3] = samples[:, 3] % 360.
 
-        # Plotting here.
+        # Plotting the results.
         if plot_walkers:
             functions.plot_walkers(sampler.chain.T, nburnin, labels)
         if plot_corner:
             functions.plot_corner(samples, labels)
         if plot_bestfit:
             self._plot_bestfit(samples, theta_fixed, fit_mstar, beam,
-                               r_min, r_max, z_type, nearest)
+                               z_type, nearest)
         if plot_residual:
             self._plot_bestfit(samples, theta_fixed, fit_mstar, beam,
-                               r_min, r_max, z_type, nearest, residual=True)
+                               z_type, nearest, residual=True)
 
         # Return the fits.
         if return_sampler:
@@ -267,10 +197,80 @@ class firstmomentcube(imagecube):
             return samples
         return np.percentile(samples, [16, 50, 84], axis=0)
 
+    def _guess_p0(self, p0=None, fit_mstar=True, z_type='thin', nearest=None):
+        """Returns estimated [p0, theta_fixed] based on the data."""
+
+        # Check the input parameters.
+        z_type = z_type.lower()
+        if z_type not in ['thin', 'conical', 'flared']:
+            raise ValueError("Can only fit 'thin', 'conical' or 'flared'.")
+        theta_fixed = self.inc if fit_mstar else self.mstar
+
+        # Find the starting positions.
+        if p0 is None:
+            if self.verbose:
+                print("WARNING: Estimating starting values. May not work.")
+            free_theta = self.mstar if fit_mstar else self.inc
+            p0 = [0.0, 0.0, free_theta, self._estimate_PA(),
+                  np.nanmedian(self.data) * 1e3]
+            if z_type == 'conical':
+                p0 += [8.0]
+                if nearest is None:
+                    p0 += [0.0]
+            elif z_type == 'flared':
+                p0 += [0.2, 1.25]
+                if nearest is None:
+                    p0 += [0.0]
+            if self.verbose:
+                print("Estimated starting positions:")
+                print(p0)
+        return p0, theta_fixed
+
+    def _calculate_ivar(self, p0, r_min=None, r_max=None):
+        """Calculate the inverse variance including radius mask."""
+        ivar = np.power(self.error, -2.0)
+        r_min = 0.0 if r_min is None else r_min
+        r_max = np.inf if r_max is None else r_max
+        rvals = self.disk_coords(p0[0], p0[1], self.inc, p0[3])[0]
+        mask = np.logical_and(rvals >= r_min, rvals <= r_max)
+        ivar[~mask] = 0.0
+        return ivar
+
+    def _optimize_p0(self, p0, *args):
+        """Optimize the starting positions."""
+
+        from scipy.optimize import minimize
+
+        # Define negative log-likelihood function.
+        def nlnL(theta):
+            return -self._lnprobability(theta, *args)
+
+        # Simple bounds for the minimization.
+        bounds = [(None, None) for _ in p0]
+        bounds[0] = (-0.5, 0.5)
+        bounds[1] = (-0.5, 0.5)
+        bounds[2] = (0.3, 3.0)
+        bounds[3] = (0.0, 360.0)
+        bounds[4] = (self.vlsr * 1e3 - 1e3, self.vlsr * 1e3 + 1e3)
+
+        res = minimize(nlnL, x0=p0, method='TNC', bounds=bounds,
+                       options={'maxiter': 100000, 'ftol': 1e-5})
+        p0 = res.x
+        p0[3] = p0[3] % 360.
+
+        if res.success:
+            print("Optimized starting positions:")
+            print(p0)
+        elif self.verbose:
+            print("WARNING: scipy.optimize did not converge.")
+            print("\t Paramaters may not be optical.\n")
+            print res
+        return p0
+
     # == emcee Functions == #
 
-    def _lnprobability(self, theta, theta_fixed, fit_mstar, beam, r_min,
-                       r_max, z_type, nearest):
+    def _lnprobability(self, theta, theta_fixed, fit_mstar, beam, z_type,
+                       nearest):
         """Log-likelihood function for a thin disk."""
 
         # Unpack the standard variables.
@@ -301,7 +301,7 @@ class firstmomentcube(imagecube):
             return -np.inf
         if not -360. < PA < 360.:
             return -np.inf
-        if not 0.0 < vlsr < 1e4:
+        if abs(vlsr - self.vlsr * 1e3) > 1e3:
             return -np.inf
         if not -0.3 <= tilt <= 0.3:
             return -np.inf
@@ -329,7 +329,7 @@ class firstmomentcube(imagecube):
         # Make the (convolved) model.
         vkep = self._get_model(x0=x0, y0=y0, inc=inc, PA=PA, vlsr=vlsr,
                                mstar=mstar, z_type=z_type, params=params,
-                               tilt=tilt, r_min=r_min, r_max=r_max, beam=beam)
+                               tilt=tilt, beam=beam)
 
         # Calculate chi-squared and return.
         lnx2 = np.power((self.data - vkep), 2) * self.ivar
@@ -337,7 +337,7 @@ class firstmomentcube(imagecube):
         return lnx2 if np.isfinite(lnx2) else -np.inf
 
     def _get_model(self, x0, y0, inc, PA, vlsr, mstar, z_type, params, tilt,
-                   r_min, r_max, beam):
+                   beam):
         """Return the Keplerian rotation model."""
         nearest = 'north' if tilt >= 0 else 'south'
         vkep = self.keplerian_profile(x0=x0, y0=y0, inc=inc, PA=PA, vlsr=vlsr,
@@ -357,7 +357,7 @@ class firstmomentcube(imagecube):
 
     # == Collapsing Functions == #
 
-    def _collapse_cube(self, method='ninth', clip=None):
+    def _collapse_cube(self, method='ninth', **kwargs):
         """
         Collapse the cube based on the method:
         first / average : Intensity weighted average velocity.
@@ -388,18 +388,22 @@ class firstmomentcube(imagecube):
         uncertainty = np.ones(vlos.shape) * self.chan
         return vlos / 1e3, uncertainty / 1e3
 
-    def _collapse_cube_quad(self):
+    def _collapse_cube_quad(self, **kwargs):
         """Parabolic fit to the pixel of peak intensity."""
         from bettermoments import quadratic
+        kwargs['linewidth'] = kwargs.pop('linewidth', None)
+        if kwargs['linewidth'] is not None:
+            kwargs['linewidth'] /= self.chan
         rms = np.nanstd([self.data[:5], self.data[-5:]])
         vlos, uncertainty = quadratic(self.data, x0=self.velax[0],
-                                      dx=self.chan, uncertainty=rms)[:2]
+                                      dx=self.chan, uncertainty=rms,
+                                      **kwargs)[:2]
         return vlos / 1e3, uncertainty / 1e3
 
     # == Plotting Functions == #
 
-    def _plot_bestfit(self, samples, theta_fixed, fit_mstar, beam,
-                      r_min, r_max, z_type, nearest, residual=False):
+    def _plot_bestfit(self, samples, theta_fixed, fit_mstar, beam, z_type,
+                      nearest, residual=False):
         """Plot the best fit moment map."""
 
         if samples.ndim == 2:
@@ -429,16 +433,18 @@ class firstmomentcube(imagecube):
 
         model = self._get_model(x0=x0, y0=y0, inc=inc, PA=PA, vlsr=vlsr,
                                 mstar=mstar, z_type=z_type, params=params,
-                                tilt=tilt, r_min=r_min, r_max=r_max, beam=beam)
+                                tilt=tilt, beam=beam)
 
         if residual:
             model -= self.data
-            levels = np.nanpercentile(model, [10, 90])
+            levels = np.where(self.ivar != 0.0, model, np.nan)
+            levels = np.nanpercentile(levels, [2, 98])
             levels = max(abs(levels[0]), abs(levels[1]))
             levels = np.array([-levels, levels])
             ticks = None
         else:
-            levels = np.nanpercentile(self.data, [2, 98])
+            levels = np.where(self.ivar != 0.0, self.data, np.nan)
+            levels = np.nanpercentile(levels, [2, 98])
             ticks = np.arange(-20, 20)
 
         import matplotlib.cm as cm
@@ -454,12 +460,8 @@ class firstmomentcube(imagecube):
                      labelpad=15)
         ax.set_aspect(1)
         ax.grid(ls=':', color='k', alpha=0.3)
-        if r_max is None:
-            ax.set_xlim(self.xaxis.max(), self.xaxis.min())
-            ax.set_ylim(self.yaxis.min(), self.yaxis.max())
-        else:
-            ax.set_xlim(1.1 * r_max, -1.1 * r_max)
-            ax.set_ylim(-1.1 * r_max, 1.1 * r_max)
+        ax.set_xlim(self.xaxis.max(), self.xaxis.min())
+        ax.set_ylim(self.yaxis.min(), self.yaxis.max())
         ax.set_xlabel('Offset (arcsec)')
         ax.set_ylabel('Offset (arcsec)')
 
@@ -477,11 +479,12 @@ class firstmomentcube(imagecube):
         if ax is None:
             fig, ax = plt.subplots()
         if levels is None:
-            levels = np.nanpercentile(self.data, [2, 98])
-            levels = np.linspace(levels[0], levels[1], 30)
+            levels = np.percentile(self.data, [2, 98]) - self.vlsr
+            levels = max(abs(levels[0]), abs(levels[1]))
+            levels = self.vlsr + np.linspace(-levels, levels, 30)
         im = ax.contourf(self.xaxis, self.yaxis, self.data, levels,
                          cmap=cm.RdBu_r, extend='both')
-        cb = plt.colorbar(im, pad=0.02, ticks=np.arange(10))
+        cb = plt.colorbar(im, pad=0.02, ticks=np.arange(100))
         cb.set_label('Line of Sight Velocity (km/s)', rotation=270,
                      labelpad=15)
         ax.set_aspect(1)
